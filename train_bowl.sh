f26e2d2879df365e903a4db88df08ac23a3aceb0
chrisyrniu <chrisyrniu@gmail.com>
2023-02-23 19:50:24 -0500

    bowl

--- train_bowl.sh
+++ train_bowl.sh
@@ -15,7 +15,7 @@ python -u train.py \
   --n_envs 1 \
   --headless 1 \
   --num_variations 1000 \
-  --cached_states_path pour_water_init_states.pkl \
+  --cached_states_path bowl_scoop_water_init_states.pkl \
   --training_steps 1500000 \
   --learning_rate 0.0003 \
   --train_freq 1 \
