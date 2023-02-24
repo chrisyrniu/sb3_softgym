8318408ea5ce08045ed7375d8857ee0a77002d0c
chrisyrniu <chrisyrniu@gmail.com>
2023-02-24 07:03:37 -0500

    add more to generate trajs

--- generate_scaled_trajs.py
+++ generate_scaled_trajs.py
@@ -1,4 +1,3 @@
-import gym
 import numpy as np
 import argparse
 import os.path as osp
@@ -9,32 +8,32 @@ from sac import SAC
 from softgym.registered_env import env_arg_dict, SOFTGYM_ENVS
 from softgym.utils.normalized_env import normalize
 from softgym.utils.visualization import save_numpy_as_gif
-import pyflex
 import pickle
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    # ['PassWater', 'PourWater', 'PourWaterAmount', 'RopeFlatten', 'ClothFold', 'ClothFlatten', 'ClothDrop', 'ClothFoldCrumpled', 'ClothFoldDrop', 'RopeConfiguration']
-    parser.add_argument('--env_name', type=str, default='DistributeWater')
+    parser.add_argument('--env_name', type=str, default='ScoopWaterRobot2D')
     parser.add_argument('--headless', type=int, default=1, help='Whether to run the environment with headless rendering')
     parser.add_argument('--num_variations', type=int, default=1, help='Number of environment variations to be generated')
-    parser.add_argument('--device', type=str, default='cpu', help='the type of device to use (cpu|cuda)')
+    parser.add_argument('--device', type=str, default='cuda', help='the type of device to use (cpu|cuda)')
     parser.add_argument('--load_file_dir', help='the path to model to test', type=str, default='save_dir/model.zip')
     parser.add_argument('--render', type=int, default=0, help='if render the tested environment')
-    parser.add_argument('--num_episodes', help='number of episodes to test', type=int, default=20)
+    parser.add_argument('--num_episodes', help='number of episodes for each evaluation setting', type=int, default=1)
     parser.add_argument('--img_size', type=int, default=720, help='Size of the recorded videos')
     parser.add_argument('--save_video', type=int, default=1, help='if save the video')
-    parser.add_argument('--save_video_dir', type=str, default='save_dir', help='Path to the saved video')
+    parser.add_argument('--save_dir', type=str, default='save_dir', help='Path to save the trajectories and videos')
     parser.add_argument('--seed', type=int, default=0)
     # Env args
-    parser.add_argument('--loader_name', type=str, default='bowl', help='the type of the loader (bowl|bucket)')
+    parser.add_argument('--loader_name', type=str, default='bucket', help='the type of the loader (bowl|bucket)')
     parser.add_argument('--water_amount_goal', type=float, default=0.60, help='The water amount goal')
     parser.add_argument('--position_goal', type=int, default=0, choices=[0, 1, 2, 3], help='0 means randomly sample positions from the workspace, 1,2,3 will give three preset position goals')
     parser.add_argument('--init_waterline', type=int, default=0, choices=[0, 1, 2, 3], help='0 means randomly sample initial waterlines, 1,2,3 will give three preset initial waterlines')
-    parser.add_argument('--pos_goal_lower', type=float, default=0.55, help='the lower bound in height of the postion goal area')
-    parser.add_argument('--pos_goal_upper', type=float, default=0.75, help='the upper bound in height of the postion goal area')
 
     args = parser.parse_args()
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
 
     env_kwargs = env_arg_dict[args.env_name]
 
@@ -50,12 +49,6 @@ if __name__ == "__main__":
     env_kwargs['water_amount_goal'] = args.water_amount_goal
     env_kwargs['eval_position_goal'] = args.position_goal
     env_kwargs['eval_init_waterline'] = args.init_waterline
-    env_kwargs['pos_goal_lower'] = args.pos_goal_lower
-    env_kwargs['pos_goal_upper'] = args.pos_goal_upper
-
-
-    if not env_kwargs['use_cached_states']:
-        print('Waiting to generate environment variations. May take 1 minute for each variation...')
 
     set_random_seed(args.seed)
     env = normalize(SOFTGYM_ENVS[args.env_name](**env_kwargs))
@@ -63,12 +56,14 @@ if __name__ == "__main__":
     env.action_space.seed(args.seed)
     model = SAC.load(path=args.load_file_dir, device=args.device, env=env)
     model.set_random_seed(args.seed)
-    print('use seed: ', args.seed)
 
     saved_info = {}
     saved_info['loader_type'] = args.loader_name
+    scale = 1
     if args.loader_name == 'bucket':
         saved_info['bucket_front_length'] = 0.08775 * 2
+        scale = 117 / saved_info['bucket_front_length']
+        saved_info['bucket_front_length'] = 117
     elif args.loader_name == 'bowl':
         saved_info['bowl_radius'] = 0.115487415
     else:
@@ -94,7 +89,6 @@ if __name__ == "__main__":
     episode_reward_for_reg = []
     frames = []
     for i in range(args.num_episodes):
-        print(i)
         done = False
         episode_reward = 0
         obs = env.reset()
@@ -149,6 +143,9 @@ if __name__ == "__main__":
         targeted_water_amounts.append(targeted_water_amount)
         rwds.append(rwd)
     
+    env._wrapped_env.close()
+    del env
+
     loader_poss = np.stack(loader_poss)
     loader_vels = np.stack(loader_vels)
     loader_rots = np.stack(loader_rots)
@@ -170,25 +167,24 @@ if __name__ == "__main__":
     saved_info['reward_trajs'] = rwds
     saved_info['episode_reward_trajs'] = np.array(episode_reward_for_reg)
 
-    with open(f'{args.loader_name}_targeted_amount_{args.water_amount_goal}_saved_trajs_seed_{args.seed}.pkl', 'wb') as handle:
-        pickle.dump(saved_info, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    scaled_keys = ['tank_height', 'tank_border_thickness', 'tank_length', 
+                'tank_width', 'loader_pos_trajs', 'loader_vel_trajs', 
+                'waterline_trajs', 'targeted_pos_trajs']
+    for key in scaled_keys:
+        saved_info[key] = saved_info[key] * scale
     
-    with open(f'{args.loader_name}_targeted_amount_{args.water_amount_goal}_saved_trajs_seed_{args.seed}.pkl', 'rb') as handle:
-        data = pickle.load(handle)
+    print('---------')
+    print('amount goal:', args.water_amount_goal, ',', 'position_goal:', args.position_goal, ',', 'init waterline', args.init_waterline)
+    mean_water_amounts = np.mean(saved_info['in_loader_percent_trajs'][:, -20:, 0], axis=1)
+    print('average water amounts from last 20 steps:', mean_water_amounts)
+    error = np.round(np.mean(mean_water_amounts - args.water_amount_goal), 5)
+    print('average error torwards the goal:', error)
 
-    # print(saved_info['waterline_trajs'])
-    print('testing results')
-    print('number of episodes:', saved_info['in_loader_percent_trajs'].shape[0])
-    print('average initial waterline:', np.mean(saved_info['waterline_trajs'][:, :10]))
-    print('episode rewards:')
-    print(episode_reward_for_reg)
-    print('mean:', np.mean(episode_reward_for_reg))
-    print('std:', np.std(episode_reward_for_reg))
-    print('average water amounts from last 20 steps:')
-    print(saved_info['in_loader_percent_trajs'][:, -20:, 0])
-    print(np.mean(saved_info['in_loader_percent_trajs'][:, -20:, 0], axis=1))
+    save_name = osp.join(args.save_dir, f'{args.loader_name}_amount_goal_{args.water_amount_goal}_pos_goal_{args.position_goal}_waterline_{args.init_waterline}_seed_{args.seed}_error_{error}.pkl')
+    with open(save_name, 'wb') as handle:
+        pickle.dump(saved_info, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     if args.save_video:
-        save_name = osp.join(args.save_video_dir, args.loader_name + '_' + str(args.water_amount_goal) + '.gif')
+        save_name = osp.join(args.save_dir, f'{args.loader_name}_amount_goal_{args.water_amount_goal}_pos_goal_{args.position_goal}_waterline_{args.init_waterline}_seed_{args.seed}_error_{error}.gif')
         save_numpy_as_gif(np.array(frames), save_name)
-        print('Video generated and save to {}'.format(save_name))
+        # print('Video generated and save to {}'.format(save_name))
---
+++ generate_scaled_trajs.sh
@@ -0,0 +1,26 @@
+export OMP_NUM_THREADS=1
+
+for water_amount_goal in 0.60 0.65 0.70 0.75 0.80
+do
+  for init_waterline in 1 2 3
+  do
+    for position_goal in 1 2 3
+    do
+      python -u generate_scaled_trajs.py \
+        --env_name ScoopWaterRobot2D \
+        --loader_name bucket \
+        --headless 1 \
+        --num_variations 1 \
+        --device cuda \
+        --load_file_dir save_dir_robot/robot_bucket_init035_area3050_s1630e1650_amount070m_s51e85_sac_her_curr_p2590a2555_buffs200_seed0/sac_her_curr_seed0_best_model \
+        --render 1 \
+        --num_episodes 1 \
+        --save_video 1 \
+        --save_dir scaled_trajs \
+        --water_amount_goal ${water_amount_goal} \
+        --position_goal ${position_goal} \
+        --init_waterline ${init_waterline} \
+        --seed 0
+    done
+  done
+done
